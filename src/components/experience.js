4859362303b799533beee63a0b5759ca84af57cb
Joeseph Rodrigues <dowhilegeek@gmail.com>
2018-10-24 21:46:23 -0700

    style experience component

--- src/components/experience.js
+++ src/components/experience.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default ({ company, title, dateRange, children }) => {
   return(
-    <div>
-      {title} @ {company}
+    <div className='experience-container'>
+      <div className='experience-header'>
+        <span><b>{title}</b> at {company}</span>
+        {dateRange}
+      </div>
       <ul>
         {children}
       </ul>
