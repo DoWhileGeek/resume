536c993f1baf481097701dfcc1bb1f9b5239ba79
Joeseph Rodrigues <dowhilegeek@gmail.com>
2018-11-04 23:57:31 -0800

    first pass at parametrizing contents

--- src/components/main.js
+++ src/components/main.js
@@ -5,32 +5,34 @@ import Section from './section'
 import Skills from './skills'
 import Experience from './experience'
 import '../styles.scss';
+import data from '../../data.json'
+
+const buildExperiences = () => {
+  console.log(data.experiences)
+  return (
+    data.experiences.map(experience => (
+    <Experience
+      key={experience.dateRange}
+      company={experience.company}
+      title={experience.title}
+      dateRange={experience.dateRange}
+    >
+      {experience.accomplishments.map(accomplishment => <li key={accomplishment}>{accomplishment}</li>)}
+    </Experience>
+    ))
+  )
+}
 
 export default () => {
   return(
     <div className='container'>
       <Title title='Joeseph Rodrigues' subtitle='Seeking a Senior Engineer position.' />
       <Section title='Skills'>
-        <Skills skills={skills} />
+        <Skills skills={data.skills} />
       </Section>
       <Section title='Work Experience'>
-        <Experience
-          company='Omadi'
-          title='Sr Software Engineer'
-          dateRange='August 2017 - Present'
-        >
-          <li>hi im a line</li>
-          <li>me too</li>
-        </Experience>
+        {buildExperiences()}
       </Section>
     </div>
   )
-}
-
-const skills = {
-  Languages: ['Python', 'Nodejs', 'HTML/CSS/JS', 'Rust', 'C/C++'],
-  Frameworks: ['React', 'React Native', 'Flask', 'GraphQL'],
-  Databases: ['PostgreSQL', 'MongoDB', 'Couchbase', 'DynamoDB'],
-  Tools: ['Vim', 'Git', 'iTerm2', 'Tmux'],
-  Infrastructure: ['AWS', 'Docker', 'Serverless'],
 }
