38e6d30c674ce2643d105412f64a0d0c09524195
Joeseph Rodrigues <dowhilegeek@gmail.com>
2018-12-19 11:40:08 -0800

    improve contact component and push it

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,27 +10,27 @@ import '../../styles/components/contact.scss'
 export default ({ skills }) => {
   return(
     <div className='contact-container'>
-      <div className='col'>
-        <div className='cell'>
-          <a href='https://linkedin.com/in/joesephrodrigues' target='_blank'>
-            <span><FontAwesomeIcon icon={faLinkedin} size='lg' color='#0077b5' /> /in/joesephrodrigues</span>
-          </a>
-        </div>
-        <div className='cell'>
-          <a href='https://twitter.com/dowhilegeek' target='_blank'>
-            <span><FontAwesomeIcon icon={faTwitter} size='lg' color='#1da1f2' /> /dowhilegeek</span>
-          </a>
-        </div>
-      </div>
-      <div className='col'>
-        <div className='cell'>
+      <div className='contact-col'>
+        <div className='contact-cell'>
           <a href='https://github.com/dowhilegeek' target='_blank'>
             <span><FontAwesomeIcon icon={faGithub} size='lg' color='#333' /> /dowhilegeek</span>
           </a>
         </div>
-        <div className='cell'>
+        <div className='contact-cell'>
+          <a href='https://linkedin.com/in/joesephrodrigues' target='_blank'>
+            <span><FontAwesomeIcon icon={faLinkedin} size='lg' color='#0077b5' /> /in/joesephrodrigues</span>
+          </a>
+        </div>
+      </div>
+      <div className='contact-col'>
+        <div className='contact-cell'>
+          <a href='https://twitter.com/dowhilegeek' target='_blank'>
+            <span><FontAwesomeIcon icon={faTwitter} size='lg' color='#1da1f2' /> /dowhilegeek</span>
+          </a>
+        </div>
+        <div className='contact-cell'>
           <a href='mailto:dowhilegeek@gmail.com' target='_blank'>
-            <span><FontAwesomeIcon icon={faEnvelope} size='lg' color='#dd4b39' /> <span className="hey">dowhilegeek@gmail.com</span></span>
+            <span><FontAwesomeIcon icon={faEnvelope} size='lg' color='#dd4b39' /> dowhilegeek@gmail.com</span>
           </a>
         </div>
       </div>
