26f3135c4b4ed2bc6faf96dc23f1e757c302bdc6
Joeseph Rodrigues <dowhilegeek@gmail.com>
2018-12-19 07:57:35 -0800

    add contact component

--- src/components/main.js
+++ src/components/main.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 import Title from './title'
 import Section from './section'
+import Contact from './contact'
 import Skills from './skills'
 import Experience from './experience'
 import '../styles.scss';
 import data from '../../data.json'
 
 const buildExperiences = () => {
-  console.log(data.experiences)
   return (
     data.experiences.map(experience => (
     <Experience
@@ -27,9 +27,15 @@ export default () => {
   return(
     <div className='container'>
       <Title title='Joeseph Rodrigues' subtitle='Seeking a Senior Engineer position.' />
+
+      <Section title='Contact'>
+        <Contact/>
+      </Section>
+
       <Section title='Skills'>
         <Skills skills={data.skills} />
       </Section>
+
       <Section title='Work Experience'>
         {buildExperiences()}
       </Section>
