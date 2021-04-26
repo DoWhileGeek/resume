b641d744b880f7e3fc6f15b04cf354cd18daaaea
Joeseph Rodrigues <dowhilegeek@gmail.com>
2021-04-26 15:40:53 -0700

    exploratory work to try out skill levels in skill matrix

--- src/components/skills.js
+++ src/components/skills.js
@@ -2,10 +2,26 @@ import React from 'react'
 
 import '../../styles/components/skills.scss'
 
-const buildCell = (skill) => (
-  <span key={skill} className="cell">
-    {skill}
-  </span>
+const ProficiencyBar = ({ level = 1 }) => (
+  <div className="proficiency-container">
+    {Array(level)
+      .fill(null)
+      .map((_, index) => (
+        <svg key={index} height="5" width="10">
+          {/*<circle r="2.5" fill="black" />*/}
+          <rect width="10" height="2.5" fill="black" />
+        </svg>
+      ))}
+  </div>
+)
+
+const buildCell = ({ skill, level }) => (
+  <div className="cell-container">
+    <span key={skill} className="cell">
+      {skill}
+    </span>
+    <ProficiencyBar level={level} />
+  </div>
 )
 
 const buildRow = ([category, skills]) => {
@@ -15,7 +31,7 @@ const buildRow = ([category, skills]) => {
     </span>
   )
 
-  return [heading, ...skills.map(buildCell)]
+  return [heading, ...skills.sort((a, b) => b.level - a.level).map(buildCell)]
 }
 
 export default ({ skills }) => {
